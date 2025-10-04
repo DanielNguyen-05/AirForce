82bfacf29013b0fcabeafba411efde8ff6b3f209
DanielNguyen-05 <nhdang231@clc.fitus.edu.vn>
2025-10-04 12:29:03 +0700

    update

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import numpy as np
 from datetime import datetime, timedelta
 import os
 
-DATA_FOLDER = 'data/2024' 
+DATA_FOLDER = 'data/2025' 
 POLLUTION_FILES = ["co.csv", "no2.csv", "ozone.csv", "pm10.csv", "pm25.csv", "so2.csv"]
 WEATHER_FILES = ["press.csv", "rh.csv", "temp.csv", "wind.csv"]
 ALL_INPUT_FILES = POLLUTION_FILES + WEATHER_FILES
-OUTPUT_CSV = "2024_data.csv" 
+OUTPUT_CSV = "training/2025_data.csv" 
 
 # --- MÔ PHỎNG DỮ LIỆU VỆ TINH TEMPO ---
 def mock_fetch_tempo_data(date, latitude, longitude):
@@ -49,9 +49,9 @@ def process_and_convert(df, filename):
     elif filename == 'temp.csv':
         df_cleaned['feature'] = 'temperature_c'
         df_cleaned['value'] = (df_cleaned['value'] - 32) / 1.8
-    elif filename == 'RH_Dewpoin.csv':
+    elif filename == 'rh.csv':
         df_cleaned['feature'] = 'humidity_perc'
-    elif filename == 'barometric_ressure.csv':
+    elif filename == 'press.csv':
         df_cleaned['feature'] = 'pressure_hpa'
     elif filename == 'wind.csv':
 
