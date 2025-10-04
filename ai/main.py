aa21d1858b147e0c4e8eb736512f20b106a7fc21
VictorNguyenLPN <quanghuy71847@gmail.com>
2025-10-05 02:06:16 +0700

    backup

--- ai/main.py
+++ ai/main.py
@@ -13,7 +13,9 @@ df = pd.read_csv("./data/mean_dataset.csv")
 df = df.head(7)
 features = ['pm25', 'pm10', 'no2', 'co', 'so2', 'o3', 'x', 'y', 'z']
 X = df[features].values
+
 X = X.reshape((1, X.shape[0], X.shape[1]))
+print(X)
 y_pred = model.predict(X)
 
 print("Predicted AQI sequence:", y_pred.flatten())
